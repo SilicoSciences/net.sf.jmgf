22323db3338e575958d7105d55701d9441ad4590
Kerner <alexanderkerner24@gmail.com>
2013-09-17 16:40:33 +0200

    MGFElement does not extend Serializable.

--- src/main/java/net/sf/jmgf/MGFElement.java
+++ src/main/java/net/sf/jmgf/MGFElement.java
@@ -1,11 +1,33 @@
 package net.sf.jmgf;
 
-import java.io.Serializable;
 import java.util.List;
 
 import net.sf.bioutils.proteomics.Peak;
 
-public interface MGFElement extends Serializable {
+/**
+ * 
+ * TODO description
+ * 
+ * <p>
+ * <b>Example:</b><br>
+ * 
+ * </p>
+ * <p>
+ * 
+ * <pre>
+ * TODO example
+ * </pre>
+ * 
+ * </p>
+ * <p>
+ * last reviewed: 2013-08-12
+ * </p>
+ * 
+ * @author <a href="mailto:alexanderkerner24@gmail.com">Alexander Kerner</a>
+ * @version 2013-08-12
+ * 
+ */
+public interface MGFElement {
 
     String getCharge();
 
